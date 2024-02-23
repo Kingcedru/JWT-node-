bd0428a2f80d41a85f9a69d087a0c92fb18793aa
Kingcedru <byishimocedrick@gmail.com>
2024-02-23 16:59:54 +0200

    creating authservice

--- authServer.js
+++ authServer.js
@@ -22,6 +22,14 @@ app.get("/posts", authenticateToken, (req, res) => {
   res.json(posts.filter((post) => post.username === req.user.name));
 });
 
+app.post("/login", (req, res) => {
+  //Authentication
+  const username = req.body.username;
+  const user = { name: username };
+  const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
+  res.json({ accessToken: accessToken });
+});
+
 function authenticateToken(req, res, next) {
   const authHeader = req.headers["authorization"];
   const token = authHeader && authHeader.split(" ")[1];
@@ -33,4 +41,4 @@ function authenticateToken(req, res, next) {
   });
 }
 
-app.listen(3000);
+app.listen(4000);
