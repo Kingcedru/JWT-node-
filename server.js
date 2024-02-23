94837973836df05cfdf642c636cc32900278cbb8
Kingcedru <byishimocedrick@gmail.com>
2024-02-23 16:01:26 +0200

    adding authentication

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
+require("dotenv").config();
+
 const express = require("express");
 const app = express();
-
 const jwt = require("jsonwebtoken");
 
 app.use(express.json());
@@ -16,16 +17,21 @@ const posts = [
   },
 ];
 
-app.get("/posts", (req, res) => {
-  res.json(posts);
+app.get("/posts", authenticateToken, (req, res) => {
+  res.json(posts.filter((post) => post.username === req.user.name));
 });
 
-app.post("/posts", (req, res) => {
-  //Authentication
-  const username = req.body.username;
-  const user = { name: username };
-  const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
-  res.json({ accessToken: accessToken });
-});
+function authenticateToken(req, res, next) {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+  if (token == null) return res.sendStatus(401);
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    console.log(err);
+    if (err) return res.sendStatus(403);
+    req.user = user;
+    next();
+  });
+}
 
 app.listen(3000);
