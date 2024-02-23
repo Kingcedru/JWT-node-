0f3d403d6e4de479e164f76fd2bdab346036d3ff
Kingcedru <byishimocedrick@gmail.com>
2024-02-23 15:24:15 +0200

    adding jwt auth

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+
+const jwt = require("jsonwebtoken");
+
+app.use(express.json());
+
+const posts = [
+  {
+    username: "Kyle",
+    title: "Post 1",
+  },
+  {
+    username: "Jim",
+    title: "Post 2",
+  },
+];
+
+app.get("/posts", (req, res) => {
+  res.json(posts);
+});
+
+app.post("/posts", (req, res) => {
+  //Authentication
+  const username = req.body.username;
+  const user = { name: username };
+  const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
+  res.json({ accessToken: accessToken });
+});
+
+app.listen(3000);
